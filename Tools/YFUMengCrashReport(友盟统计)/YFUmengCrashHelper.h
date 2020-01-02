7c5b8062cf957c12ebc1bdda43224198ff28403b
xuqee <910321599@qq.com>
2020-01-02 10:32:16 +0800

    添加友盟错误日志

---
+++ Tools/YFUMengCrashReport(友盟统计)/YFUmengCrashHelper.h
@@ -0,0 +1,19 @@
+//
+//  YFUmengCrashHelper.h
+//  YFTools
+//
+//  Created by ggg on 2020/1/2.
+//  Copyright © 2020 QYHB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YFUmengCrashHelper : NSObject
+
++ (void)addUMENGSettingsWithAppKey:(NSString *)appKey ;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tools/YFUMengCrashReport(友盟统计)/YFUmengCrashHelper.m
@@ -0,0 +1,31 @@
+//
+//  YFUmengCrashHelper.m
+//  YFTools
+//
+//  Created by ggg on 2020/1/2.
+//  Copyright © 2020 QYHB. All rights reserved.
+//
+
+#import "YFUmengCrashHelper.h"
+
+@implementation YFUmengCrashHelper
+
++ (void)addUMENGSettingsWithAppKey:(NSString *)appKey{
+#ifdef DEBUG
+    NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
+#else
+    [UMConfigure setEncryptEnabled:YES];//打开加密传输
+    [UMConfigure initWithAppkey:appKey channel:@"App Store"];
+    [MobClick setCrashReportEnabled:YES];
+#endif
+}
+
+////测试奔溃定位 通过 “image lookup -address 可能的地址” 命令去定位可能崩溃的地方
+//NSException *exception = [NSException exceptionWithName:@"异常" reason:@"崩溃吧孩子" userInfo:@{@"Hello":@"World"}];
+//  @throw exception ;
+static void uncaughtExceptionHandler(NSException *exception) {
+    NSLog(@"%@\n%@", exception, [exception callStackSymbols]);
+}
+
+
+@end
